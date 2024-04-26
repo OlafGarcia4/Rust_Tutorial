bf1cb8f6cad27f12c16ec016e6edd21657fa05a5
OlafGarcia4 <olaf_g04@live.com>
2024-04-26 16:07:20 -0500

    Code examples for Ch 7 and 8.

--- src/Ch7_Collections_and_Data_stucts/src/main.rs
+++ src/Ch7_Collections_and_Data_stucts/src/main.rs
@@ -1,3 +1,54 @@
 fn main() {
-    println!("Hello, world!");
+    println!("\n\n");
+    let mut vector = vec![1, 2, 3, 4, 5, 5, 3, 4, 4] ;
+    let result = median(&mut vector);
+    println!("The median is : {}", result);   
+    mode(&mut vector);
+    print!("Please enter a word or phrase: ");
+    io::stdout().flush(); // since print! doe not create a new line the stdout will need to be refreshed as it is buffered. See the documentation https://doc.rust-lang.org/std/macro.print.html 
+    letter_counter();
+    println!("\n\n");
+    
+}
+
+use std::io;
+use std::io::Write;
+
+fn letter_counter() {
+
+    let mut user_input = String::new();
+    
+    io::stdin().read_line(&mut user_input).expect("Please enter a UTF-8 string");
+    let mut map = HashMap::new();
+    for c in user_input.chars() {
+        let count = map.entry(c).or_insert(0);
+        *count += 1;
+    }
+    println!("The count of letters: {:?}", map);
+}
+
+fn median(list : &mut Vec<i32>)-> i32 {
+    list.sort();
+    let middle = list.len()/2;
+    *list.get(middle).unwrap()
+    
+}
+
+use std::collections::HashMap;
+
+fn mode(list : &mut Vec<i32>) {
+    let mut map = HashMap::new();
+    let mut max = 0;
+    let mut key = 0;
+    for num in list.iter() {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+
+        if *count > max {
+            max = *count;
+            key = *num;
+        }
+
+    }
+    println!("The mode is [{key}: {max}]");
 }
--- src/Ch8_Error_handeling/src/main.rs
+++ src/Ch8_Error_handeling/src/main.rs
@@ -1,3 +1,23 @@
+//use std::env;
+use std::fs::File;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    //env::set_var("RUST_BACKTRACE", "1");
+    let v = vec![1, 2, 3];
+    let greeting_file_result = File::open("hello.txt");
+    //v[99];
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error)=> panic!("Problem opening file {:?}", error)
+    } ;
+    println!("No errors, yay!");
+}
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
